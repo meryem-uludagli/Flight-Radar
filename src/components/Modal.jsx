f1f1b6331e9c8d40909e803992bcf7fe2ecf9d68
meryem-uludagli <meryemuslu2035@gmail.com>
2024-12-17 18:55:46 +0200

    add

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-
 const Modal = ({ id, close }) => {
-  return <div>Modal</div>;
+  return (
+    <div className="modal-outer">
+      <div className="modal-inner">
+        <div className="close-wrapper">
+          <button onClick={close}>X</button>
+        </div>
+        <h1>{id}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
